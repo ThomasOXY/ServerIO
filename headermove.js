b7e3ece96d48a68154f29ab66df9b95a90b01988
OXY_CZ <spaletomas@seznam.cz>
2021-12-22 10:05:47 +0100

    Added script + animation

---
+++ headermove.js
@@ -0,0 +1,18 @@
+ function home(){
+       document.body.classList.add("animate-out");
+       setTimeout(() => {   window.location.href = 'index.html'; }, 300);
+      
+   }
+   
+   function servers(){
+    document.body.classList.add("animate-out");
+    setTimeout(() => {   window.location.href = 'servers.html'; }, 300);
+   
+}
+
+function first(){
+    document.body.classList.add("animate-out");
+    setTimeout(() => {   window.location.href = 'first.html'; }, 300);
+   
+}
+
